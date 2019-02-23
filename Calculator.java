c42c1bbcf605114d1e38a8c55fa33beb88de4e4f
RaghavJeevendra <raghavj0505@gmail.com>
2019-02-22 19:02:49 -0700

    calculator.java file added

---
+++ Calculator.java
@@ -0,0 +1,97 @@
+
+package cse360assign2;
+/*============================================================================
+Name        : Calculator.java
+Author      : Raghav Jeevendra
+Date	    : Feb 22, 2019
+Description : 
+   Implementation of Calculator to do basic functionalities like addition, 
+   subtraction, multiplication and division 
+  
+============================================================================*/
+
+/**
+ * @author Raghav Jeevendra
+ * @ClassID 421
+ * @Assignment number 2
+ * 
+ *             <p>
+ *             Implementation of Calculator to do basic functionalities like addition, 
+ * 			   subtraction, multiplication and division 
+ * 
+ * 			   This class has functions:
+ * 			   add function is used to add number to total
+ * 			   subtract function is used to subtract number from total
+ * 			   multiply function is used to multiply total by number
+ * 			   divide function is used to divide total by number
+ *
+ */
+
+public class Calculator {
+
+	/**
+	 * 
+	 * Integer total
+	 */
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * 
+	 * <p>Function return total
+	 *
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * 
+	 * @param value 
+	 * <p>adds value to total
+	 *
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value 
+	 * <p>subtracts value from total
+	 *
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value 
+	 * <p>multiplies total by value
+	 *
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value 
+	 * <p>divides total by value
+	 *
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * <p>returns the history of operations
+	 *
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
